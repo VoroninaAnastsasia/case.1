18452d4e3fd9d965015e36e6fdb9acaafddb2dff
AlenaLeonovaa <a.leonova5@g.nsu.ru>
2026-09-13 15:20:44 +0700

    Update text_utils.py

--- infrastructure/text_utils.py
+++ infrastructure/text_utils.py
@@ -1,15 +1,18 @@
-def splitSentences(text: str) -> list[str]:
-    """
-    Разбивает текст на предложения.
+import re
 
-    используются знаки . ! ?
-    """
-    sentences = re.split(r'[.!?]+', text)
-    return [sentence.strip() for sentence in sentences if sentence.strip()]
+
+def splitSentences(text: str) -> list[str]:
+  """
+  Разбивает текст на предложения.
+
+  Используются знаки . ! ?
+  """
+  sentences = re.split(r'[.!?]+', text)
+  return [sentence.strip() for sentence in sentences if sentence.strip()]
 
 
 def splitWords(text: str) -> list[str]:
-    """
-    Извлекает слова из текста.
-    """
-    return re.findall(r'\b[\wёЁа-яА-Яa-zA-Z]+\b', text, re.UNICODE)
+  """
+  Извлекает слова из текста.
+  """
+  return re.findall(r'\b[\wёЁа-яА-Яa-zA-Z]+\b', text, re.UNICODE)
