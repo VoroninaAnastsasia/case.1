775470db5d3aabd6b4ffcdccbf9008568ad45817
VoroninaAnastsasia <a.voronina3@g.nsu.ru>
2026-09-13 14:37:44 +0700

    Update services.py

--- application/services.py
+++ application/services.py
@@ -1,1 +1,22 @@
+from infrastructure.syllable_counters import (
+  countSyllablesRu,
+  countSyllablesEn,
+  countSyllablesDe,
+  countSyllablesFr
+)
 
+from infrastructure.language_detector import detectLanguage
+from infrastructure.sentiment import analyzeSentiment
+
+
+def getServices():
+  return {
+    "language_detector": detectLanguage,
+    "syllable_counters": {
+      "ru": countSyllablesRu,
+      "en": countSyllablesEn,
+      "de": countSyllablesDe,
+      "fr": countSyllablesFr
+    },
+    "sentiment_analyzer": analyzeSentiment
+  }
